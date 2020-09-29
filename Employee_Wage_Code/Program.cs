c335111ac5d669178deb7ee07fc3f244d54b7a1f
Satantra Tewari <satantra1508@gmail.com>
2020-09-29 13:01:26 +0530

    [satantra] add. wages calculated till minimum working hours/days in month

--- Employee_Wage_Code/Program.cs
+++ Employee_Wage_Code/Program.cs
@@ -10,17 +10,19 @@ namespace Employee_Wage_Code
         public const int IS_PART_TIME = 1;
         public const int EMP_RATE_PER_HR= 20;
         public const int NUM_WRKNG_DAYS = 2;
+        public const int MAX_HRS = 10;
         static void Main(string[] args)
         {
-            // variables
-            int empHr=0;
-            int empWage=0;
-            int totalEmpWage = 0;
-            for (int day = 0; day < NUM_WRKNG_DAYS; day++)
+            //variables
+            int empHr = 0;
+            int totalEmpHr = 0;
+            int totalWrkDay = 0;
+            while (totalEmpHr <= MAX_HRS && totalWrkDay < NUM_WRKNG_DAYS)
             {
+                totalWrkDay++;
                 Random random = new Random();
                 int empChk = random.Next(0, 3);
-                switch(empChk)
+                switch (empChk)
                 {
                     case IS_PART_TIME:
                         empHr = 4;
@@ -32,11 +34,11 @@ namespace Employee_Wage_Code
                         empHr = 0;
                         break;
                 }
-                empWage = empHr * EMP_RATE_PER_HR;
-                totalEmpWage += empWage;
-                Console.WriteLine(" Employee Wage is: " + empWage);
+                totalEmpHr += empHr;
+                Console.WriteLine(" Days: " + totalWrkDay + " Employee Hours: " + empHr);
             }
-            Console.WriteLine(" Total Employee Wage: " + totalEmpWage);
+            int totEmpWage = totalEmpHr * EMP_RATE_PER_HR;
+            Console.WriteLine(" Total Employee Wage: " + totEmpWage);
         }
     }
 }
